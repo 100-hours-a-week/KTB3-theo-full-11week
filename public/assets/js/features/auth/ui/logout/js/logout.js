a84a2ba284613d0ffff9ed51c66ebf73373586d9
yeongmin <hwongyoungmin@gmail.com>
2025-11-26 22:35:34 +0900

    ADD: logout 컴포넌트 requestLogout API 요청 추가

--- public/assets/js/features/auth/ui/logout/js/logout.js
+++ public/assets/js/features/auth/ui/logout/js/logout.js
@@ -1,5 +1,6 @@
 import { emit } from "../../../../../shared/lib/eventBus.js";
 import { login } from "../../login/js/login.js";
+import { requestLogout } from "../../../../../shared/lib/api/user-api.js";
 import { clearPathHistory } from "../../../../../shared/lib/router.js";
 import { accessTokenStore } from "../../../../../shared/lib/jwt/access-token.js";
 
@@ -7,6 +8,7 @@ export function logout() {
     localStorage.clear();
     clearPathHistory();
     accessTokenStore.clear();
+    requestLogout();
     emit('user:logout', {});
     return login();
 
